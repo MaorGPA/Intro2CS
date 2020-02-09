289bd8b6dc49d46c7e5c62168c3bc24fe5058a69
Maor Shoshan <maor.shoshan@riskified.com>
2020-02-09 22:45:17 +0200

    init

---
+++ ex0/ex0.py
@@ -0,0 +1,8 @@
+###############################################################
+# FILE : ex0.py
+# WRITER : Maor Shoshan
+# EXERCISE : intro2cs ex0 2020
+# DESCRIPTION : A simple program that prints "Hello World!" to
+# the standard output (screen).
+###############################################################
+print("Hello World!")
