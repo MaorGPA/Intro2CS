b143fb15f2eab84ca95f9330c9e29520bf9d09ab
Maor Shoshan <maor.shoshan@riskified.com>
2020-02-09 23:43:01 +0200

    math

---
+++ ex1/math_print.py
@@ -0,0 +1,42 @@
+import math
+
+
+def golden_ratio():
+    print((1 + 5 ** 0.5) / 2)
+
+
+def six_square():
+    print(6 ** 2)
+
+
+def hypotenuse():
+    print(math.sqrt(5 ** 2 + 12 ** 2))
+
+
+def pi():
+    print(math.pi)
+
+
+def e():
+    print(math.exp(1))
+
+
+def squares_area():
+    print(4 ** 1,
+          4 ** 2,
+          4 ** 3,
+          4 ** 4,
+          4 ** 5,
+          4 ** 6,
+          4 ** 7,
+          4 ** 8,
+          4 ** 9,
+          4 ** 10)
+
+
+golden_ratio()
+six_square()
+hypotenuse()
+pi()
+e()
+squares_area()
