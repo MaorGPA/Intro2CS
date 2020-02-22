983f62be3254eae1d5f32546512a73c67c9625b0
Maor Shoshan <maor.shoshan@riskified.com>
2020-02-22 19:07:29 +0200

    quadric_equation

--- ex2/largest_and_smallest.py
+++ ex2/largest_and_smallest.py
@@ -1,20 +1,22 @@
 def largest_and_smallest(number_1, number_2, number_3):
-    # This function returns the largest number and then the smallest number out of the three arguments
     number_1_greater_number_2 = number_1 >= number_2
     number_1_greater_number_3 = number_1 >= number_3
     number_2_greater_number_3 = number_2 >= number_3
 
     if number_1_greater_number_2:
         if number_2_greater_number_3:
-            return number_1, number_3
+            largest, smallest = number_1, number_3
         elif number_1_greater_number_3:
-            return number_1, number_2
+            largest, smallest = number_1, number_2
         else:
-            return number_3, number_2
+            largest, smallest = number_3, number_2
     elif number_2_greater_number_3:
         if number_1_greater_number_3:
-            return number_2, number_3
+            largest, smallest = number_2, number_3
         else:
-            return number_2, number_1
+            largest, smallest = number_2, number_1
     else:
-        return number_3, number_1
+        largest, smallest = number_3, number_1
+
+    return largest, smallest
+
---
+++ ex2/quadratic_equation.py
@@ -0,0 +1,31 @@
+def quadric_equation(a, b, c):
+    # a, b and c are the coefficients of a quadratic equation:
+    # a * (x ** 2) + (b * x) + c = 0
+    # x1, x2 are the equation solutions
+
+    a_doubled = 2 * a
+    discriminant = (b ** 2) - (2 * a_doubled * c)
+
+    if discriminant == 0:
+        x1 = -b / a_doubled
+        x2 = None
+    elif discriminant > 0:
+        discriminant_square_root = discriminant ** 0.5
+        x1 = (-b + discriminant_square_root) / a_doubled
+        x2 = (-b - discriminant_square_root) / a_doubled
+    else:
+        x1 = x2 = None
+
+    return x1, x2
+
+
+def quadric_equation_user_input():
+    a, b, c = input("Insert coefficients a, b and c: ").split()
+    x1, x2 = quadric_equation(float(a), float(b), float(c))
+
+    if x1 is None:
+        print("The equation has no solutions")
+    elif x2 is None:
+        print("The equation has 1 solution:", x1)
+    else:
+        print("The equation has 2 solutions:", x1, "and", x2)
